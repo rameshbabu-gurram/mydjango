d76751c63155b1351968fb14c5a129cc15056499
ramesh <ramesh@localhost.localdomain>
2020-04-11 18:20:57 +0530

    added the required folders and files

--- tracker/tracker/views.py
+++ tracker/tracker/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import generic
-from . import models
+
 
 class IndexView(generic.TemplateView):
-    template_name = 'regular/index.html'
+    template_name = 'tracker/index.html'
